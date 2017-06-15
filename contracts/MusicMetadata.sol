f0aef0623e552b6e429fcfa7bee62b113f1be539
wpank <will.pankiewicz@gmail.com>
2017-06-15 17:41:58 -0500

    metadata fields

--- contracts/MusicMetadata.sol
+++ contracts/MusicMetadata.sol
@@ -2,4 +2,14 @@ pragma solidity ^0.4.11;
 
 contract MusicMetadata{
 
+  struct Metadata{
+    bytes32 title;
+    bytes32 artist;
+    bytes32 album;
+    uint256 year;
+    uint256 tracknumber;
+    uint256 track;
+    bytes32 encoder;
+  }
+
 }
