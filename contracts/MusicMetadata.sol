386925b4d66f5a64ed7b9cc94a8445b56940f82f
wpank <will.pankiewicz@gmail.com>
2017-06-15 17:59:02 -0500

    addMetadata function

--- contracts/MusicMetadata.sol
+++ contracts/MusicMetadata.sol
@@ -15,4 +15,19 @@ contract MusicMetadata{
     bytes32 songhash;
   }
 
+  function addMetadata(bytes32 _title, bytes32 _artist, bytes32 _album, uint256 _year, uint256 _tracknumber, uint256 _track, bytes32 _encoder, bytes32 _songhash) returns (bool success){
+    Metadata memory newMetadata;
+    newMetadata.title = _title;
+    newMetadata.artist = _artist;
+    newMetadata.album = _album;
+    newMetadata.year = _year;
+    newMetadata.tracknumber = _tracknumber;
+    newMetadata.track = _track;
+    newMetadata.encoder = _encoder;
+    newMetadata.songhash = _songhash;
+
+    songs.push(newMetadata);
+    return true;
+  }
+
 }
