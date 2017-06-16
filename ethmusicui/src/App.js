f31cd2e78b0a746ab377ec8478d6187395df40bd
wpank <will.pankiewicz@gmail.com>
2017-06-16 11:30:55 -0500

    Added logo

--- ethmusicui/src/App.js
+++ ethmusicui/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import _ from 'lodash';
+
+var ETH_CLIENT = new Web3(new Web3.providers.HttpProvider)
 
 class App extends Component {
   render() {
@@ -8,11 +12,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Metadata</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
